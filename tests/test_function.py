d5651454f98ef0a952f7b073851c4606c743c50e
maxim-f1 <maxconal228@gmail.com>
2024-08-15 23:51:10 +0300

    Add tests

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,8 +1,11 @@
+from random import randint
+
 import pytest
 
 from src.main import function
 
 
 @pytest.mark.asyncio
-async def test_success():
-    assert await function(2) == 4
+@pytest.mark.parametrize('a', [randint(-255, 255) for _ in range(20)])
+async def test_success(a):
+    assert await function(a) == a * 2
